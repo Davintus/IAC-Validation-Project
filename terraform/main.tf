931e8c6a3ceaed018afc07ccd96b86282ec91e72
Davintus <91970837+Davintus@users.noreply.github.com>
2025-07-02 15:33:53 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
@@ -17,7 +16,6 @@ resource "azurerm_resource_group" "rg" {
   location = "westeurope"
 }
 
-# Define Log Analytics Workspace for oms_agent
 resource "azurerm_log_analytics_workspace" "example" {
   name                = "python-app-law"
   location            = azurerm_resource_group.rg.location
@@ -26,40 +24,57 @@ resource "azurerm_log_analytics_workspace" "example" {
   retention_in_days   = 30
 }
 
+# Create a disk encryption set (required for CKV_AZURE_117)
+resource "azurerm_disk_encryption_set" "example" {
+  name                = "python-app-des"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  key_vault_key_id    = azurerm_key_vault_key.example.id # Replace with your Key Vault Key ID
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "python-app-aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "pythonapp"
-  sku_tier            = "Standard" # Fixes CKV_AZURE_170
+  sku_tier            = "Paid"
+  local_account_disabled = true # Fixes CKV_AZURE_141
+  private_cluster_enabled = true # Fixes CKV_AZURE_115
+  automatic_channel_upgrade = "patch" # Fixes CKV_AZURE_171
 
   default_node_pool {
-  name                = "default"
-  vm_size             = "Standard_D2_v2"
-  os_disk_type        = "Ephemeral" # Fixes CKV_AZURE_226
-  max_pods            = 50          # Fixes CKV_AZURE_168
-  min_count           = 1           # Minimum number of nodes
-  max_count           = 3           # Maximum number of nodes
-  node_count          = 2           # Initial number of nodes
-}
+    name                = "default"
+    vm_size             = "Standard_D2_v2"
+    os_disk_type        = "Ephemeral"
+    max_pods            = 50
+    min_count           = 1
+    max_count           = 3
+    node_count          = 2
+    enable_host_encryption = true # Fixes CKV_AZURE_227
+    disk_encryption_set_id = azurerm_disk_encryption_set.example.id # Fixes CKV_AZURE_117
+  }
 
   identity {
     type = "SystemAssigned"
   }
 
   network_profile {
-    network_plugin = "azure" # Fixes CKV2_AZURE_29
-    network_policy = "azure" # Fixes CKV_AZURE_7
+    network_plugin = "azure"
+    network_policy = "azure"
   }
 
   api_server_access_profile {
-    authorized_ip_ranges = ["10.3.0.0/16"] # Fixes CKV_AZURE_6
+    authorized_ip_ranges = ["10.3.0.0/16"]
   }
 
-  azure_policy_enabled = true # Fixes CKV_AZURE_116
+  azure_policy_enabled = true
 
   oms_agent {
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id # Fixes CKV_AZURE_4
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+  }
+
+  key_vault_secrets_provider {
+    secret_rotation_enabled = true # Fixes CKV_AZURE_172
   }
 }
 
