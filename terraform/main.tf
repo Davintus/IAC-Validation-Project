5c0250c0c9c71834fe9bb5cf1001805f17be1600
Davintus <91970837+Davintus@users.noreply.github.com>
2025-07-02 15:21:31 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,32 +17,41 @@ resource "azurerm_resource_group" "rg" {
   location = "westeurope"
 }
 
+# Define Log Analytics Workspace for oms_agent
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "python-app-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "python-app-aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "pythonapp"
-  sku_tier            = "Paid" # Fixes CKV_AZURE_170
+  sku_tier            = "Standard" # Fixes CKV_AZURE_170
 
   default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_D2_v2"
-    enable_auto_scaling = true
-    max_pods            = 50    # Fixes CKV_AZURE_168
-    os_disk_type        = "Ephemeral" # Fixes CKV_AZURE_226
-  }
+  name                = "default"
+  vm_size             = "Standard_D2_v2"
+  os_disk_type        = "Ephemeral" # Fixes CKV_AZURE_226
+  max_pods            = 50          # Fixes CKV_AZURE_168
+  min_count           = 1           # Minimum number of nodes
+  max_count           = 3           # Maximum number of nodes
+  node_count          = 2           # Initial number of nodes
+}
 
   identity {
     type = "SystemAssigned"
   }
-}
-   
+
   network_profile {
     network_plugin = "azure" # Fixes CKV2_AZURE_29
     network_policy = "azure" # Fixes CKV_AZURE_7
   }
-  
+
   api_server_access_profile {
     authorized_ip_ranges = ["10.3.0.0/16"] # Fixes CKV_AZURE_6
   }
@@ -51,8 +60,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   oms_agent {
     log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id # Fixes CKV_AZURE_4
-  }+
- 
+  }
+}
+
 output "kube_config" {
   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
